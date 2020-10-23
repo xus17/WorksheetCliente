8443b34299ae7c6e56cc7ac01ec5f4682c9a5191
Jesús Manuel González Santiago <47830898+xus17@users.noreply.github.com>
2020-10-23 13:18:43 +0200

    Create script.js

---
+++ Tema2/Worksheet6/script.js
@@ -0,0 +1,4 @@
+document.write("<br>El número de imágenes es: "+document.images.length);
+document.write("<br>El id de la primera imagen es: "+document.getElementsByTagName('img')[0].id);
+document.write("<br>El número de links de la pagina es: "+document.links.length);
+document.title="ARRIBA ESPAÑA";
