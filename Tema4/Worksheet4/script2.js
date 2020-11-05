e196c927d79ff88ca2c5824bc2844ec73d2a8858
Jesús Manuel González Santiago <47830898+xus17@users.noreply.github.com>
2020-11-05 18:54:48 +0100

    Create script2.js

---
+++ Tema4/Worksheet4/script2.js
@@ -0,0 +1,34 @@
+function AumentarTamano(){
+  document.getElementById("p1").style.fontSize ="50px";
+  document.getElementById("p2").style.fontSize ="50px";
+  document.getElementById("p3").style.fontSize ="50px";
+  document.getElementById("p4").style.fontSize ="50px";
+}
+
+function DisminuirTamano(){
+  document.getElementById("p1").style.fontSize ="5px";
+  document.getElementById("p2").style.fontSize ="5px";
+  document.getElementById("p3").style.fontSize ="5px";
+  document.getElementById("p4").style.fontSize ="5px";
+}
+
+function TamanoNormal(){
+  document.getElementById("p1").style.fontSize ="16px";
+  document.getElementById("p2").style.fontSize ="16px";
+  document.getElementById("p3").style.fontSize ="16px";
+  document.getElementById("p4").style.fontSize ="16px";
+}
+
+function TextoJustificada(){
+  document.getElementById("p1").style.textAlign="center";
+  document.getElementById("p2").style.textAlign="center";
+  document.getElementById("p3").style.textAlign="center";
+  document.getElementById("p4").style.textAlign="center";
+}
+
+function TextoIzda(){
+  document.getElementById("p1").style.textAlign="left";
+  document.getElementById("p2").style.textAlign="left";
+  document.getElementById("p3").style.textAlign="left";
+  document.getElementById("p4").style.textAlign="left";
+}
