092eefb5855dfccaf18923ef26a980b28eef33b0
Jesús Manuel González Santiago <47830898+xus17@users.noreply.github.com>
2020-11-05 18:54:30 +0100

    Create script.js

---
+++ Tema4/Worksheet4/script.js
@@ -0,0 +1,30 @@
+function TemaNormal(){
+  document.getElementById('html').style.height = "100vh";
+  document.getElementById('html').style.borderLeft = "10vw solid #17B5CC"
+  document.getElementById('body').style.backgroundColor = '#FFFAC5'
+  document.getElementById('div1').style.backgroundColor = '#c2b7d4';
+  document.getElementById('div1').style.boxShadow = '0px 0px 1px 1px #6F00B1';
+  document.getElementById('a1').style.color = 'blue';
+  document.getElementById('a1').style.fontWeight = 'bold';
+  document.getElementById('a1').style.textDecoration = 'none';
+  document.getElementById('a2').style.color = 'blue';
+  document.getElementById('a2').style.fontWeight = 'bold';
+  document.getElementById('a2').style.textDecoration = 'none';
+  document.getElementById("p4").style.border="solid #000";
+  document.getElementById("p4").style.backgroundColor = '#FFEE3F';
+  document.getElementById('p3').style.display = 'block';
+}
+
+function TemaMinimalista(){
+  document.getElementById('html').style.borderLeft = "none";
+  document.getElementById('body').style.backgroundColor = '#fff';
+  document.getElementById('div1').style.backgroundColor = '#fff';
+  document.getElementById('div1').style.boxShadow = 'none';
+  document.getElementById('a1').style.color = 'blue';
+  document.getElementById('a1').style.textDecoration = 'underline';
+  document.getElementById('a2').style.color = 'blue';
+  document.getElementById('a2').style.textDecoration = 'underline';
+  document.getElementById("p4").style.border="none";
+  document.getElementById("p4").style.backgroundColor = '#fff';
+  document.getElementById('p3').style.display = 'none';
+}
